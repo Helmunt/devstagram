54b6a651c1a5eb23e73ae2bc6e2a7405d3c2b9cb
Helmunt <hhernandezro@gmail.com>
2022-08-16 16:59:37 -0500

    Clase 16Ago2022

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Devstagram</title>
+        <title>Devstagram @yield('titulo')</title>
+        @vite('/resources/css/app.css')
     </head>
     <body>
         <nav>
