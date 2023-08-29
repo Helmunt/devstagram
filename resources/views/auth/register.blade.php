7bee0e91ed3c9f777154a408bcdd64c477c4aaaa
Helmunt <hhernandezro@gmail.com>
2023-08-29 15:46:05 -0500

    Formulario Registro
    Creación del formulario de registro

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Registrate en Devstagram
+@endsection
+
+@section('contenido')
+    <div class="md:flex md:justify-center md:gap-10 md:items-center">
+        <div class="md:w-6/12  p-5 mb">
+            <img src="{{ asset('img/registrar.jpg') }}" alt="Imagen registro usuarios">
+        </div>
+        <div class="md:w-4/12 mr-5 ml-5 bg-white p-6 rounded-lg shadow-xl ">
+            <form action="">
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-black">
+                        Nombre
+                    </label>
+                    <input
+                        id="name"
+                        name="name"
+                        type="text"
+                        placeholder="Ingresa tu nombre"
+                        class="border p-3 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-black">
+                        UserName
+                    </label>
+                    <input
+                        id="username"
+                        name="username"
+                        type="text"
+                        placeholder="Tu nombre de usuario"
+                        class="border p-3 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-black">
+                        E-mail
+                    </label>
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="Tu Correo Elecentronico"
+                        class="border p-3 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-black">
+                        Password
+                    </label>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Password de Registro"
+                        class="border p-3 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-black">
+                        Confirmar Password
+                    </label>
+                    <input
+                        id="password_confirmation"
+                        name="password_confirmation"
+                        type="password"
+                        placeholder="Repite tu password"
+                        class="border p-3 w-full rounded-lg"
+                    />
+                </div>
+                <input
+                    type="submit"
+                    value="Crear cuenta"
+                    class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full
+                    p-3 text-white rounded-lg"
+                />
+            </form>
+        </div>
+    </div>
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,11 +11,13 @@
         <header class="p-5 border-b bg-white shadow">
             <div class="container mx-auto flex justify-between items-center">
                 <h1 class="text-3xl font-black">
-                    DevStagram
+                    <a href="/">DevStagram</a>
                 </h1>
                 <nav class="flex gap-2 items-center">
                     <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Crear Cuenta </a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="/crear-cuenta">
+                        Crear Cuenta
+                    </a>
                 </nav>
         </div>
         </header>
@@ -23,9 +25,11 @@
             <h2 class="font-black text-center text-3xl mb-10">
                 @yield('titulo')
             </h2>
-            @yield('contenido')
+            <div class="text-xs">
+                @yield('contenido')
+            </div>
         </main>
-        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+        <footer class="text-center mt-10 text-sm p-5 text-gray-500 font-bold uppercase">
             DevStagram - Todos los derechos reservados
             {{ now()->year }}
         </footer>
