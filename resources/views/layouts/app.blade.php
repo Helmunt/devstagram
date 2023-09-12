263846eede752aa0dc2f433eb42d605a5ead13cd
Helmunt Hernandez <hhernandezro@gmail.com>
2023-09-12 06:58:18 -0500

    Instalación Dropzone

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        @stack('styles')
+        <link rel="stylesheet" href="{{ asset('css/app.css')}}">
         <title>Devstagram - @yield('titulo')</title>
+        <script src="{{ asset('js/app.js')}}"></script>
         @vite('resources/css/app.css')
+        @vite('resources/js/app.js')
 
     </head>
     <body class="bg-gray-100">
